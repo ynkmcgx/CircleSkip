a12f38517641812a156904306560d2fc39786324
ynkmcgx <404264279@qq.com>
2016-06-24 15:20:00 +0800

    Initial commit

---
+++ app/src/androidTest/java/circleskipapp/com/example/cgx_pc/circleskip/ApplicationTest.java
@@ -0,0 +1,13 @@
+package circleskipapp.com.example.cgx_pc.circleskip;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
