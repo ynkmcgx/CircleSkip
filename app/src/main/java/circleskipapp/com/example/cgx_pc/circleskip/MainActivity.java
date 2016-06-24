25b4fdf2ca6ccdec54eebc38edb50825e9d25525
ynkmcgx <404264279@qq.com>
2016-06-24 17:16:31 +0800

    modifie the code2

--- app/src/main/java/circleskipapp/com/example/cgx_pc/circleskip/MainActivity.java
+++ app/src/main/java/circleskipapp/com/example/cgx_pc/circleskip/MainActivity.java
@@ -1,16 +1,50 @@
 package circleskipapp.com.example.cgx_pc.circleskip;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import CustomView.CircleSkip;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CircleSkip ck;
+    private float x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //CircleSkip first push on git
+        ck = (CircleSkip) findViewById(R.id.ck);
+        //this is a customview named circleSkip
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <=360 ; i++) {
+                    x = i;
+                    ck.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            ck.setX(x);
+                        }
+                    });
+                    // 3000/360~=8.3333333……,so i obtain 8 as sleeptime;
+                    SystemClock.sleep(8);
+                }
+            }
+        }).start();
 
-        //测试Github提交代码
+        //act if user click ck,excute startAcitvity
+        ck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"startAcitivty",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
